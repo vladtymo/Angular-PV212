24c0d7ca8b77e5cb90aaa89bb86ed5b4e23ecf5f
Vlad Tymoshchuk <tymo.vlad@gmail.com>
2024-09-30 18:27:38 +0300

    Move user list to component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,12 @@ import { RouterOutlet } from '@angular/router';
 import { UserCardComponent } from "./user-card/user-card.component";
 import { IUser, USERS } from './user';
 import { UserFormComponent } from "./user-form/user-form.component";
+import { UserListComponent } from "./user-list/user-list.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, UserCardComponent, UserFormComponent],
+  imports: [RouterOutlet, UserCardComponent, UserFormComponent, UserListComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
@@ -15,25 +16,7 @@ export class AppComponent {
   title = 'Angular-PV212';
   name = "Vlad Tymoshchuk";
 
-  users: IUser[] = USERS;
-
-  // properties and methods
   changeName() {
     this.name += '!';
-  }
-
-  createUser(user: IUser) {
-    this.users.push(user);
-  }
-
-  deleteItem(id: number) {
-    let index = this.users.findIndex(x => x.id === id);
-
-    if (index !== -1)
-      this.users.splice(index, 1);
-  }
-
-  clearAll() {
-    this.users = [];
   }
 }
---
+++ src/app/user-list/user-list.component.ts
@@ -0,0 +1,32 @@
+import { Component } from '@angular/core';
+import { UserCardComponent } from "../user-card/user-card.component";
+import { UserFormComponent } from "../user-form/user-form.component";
+import { IUser, USERS } from '../user';
+
+@Component({
+  selector: 'app-user-list',
+  standalone: true,
+  imports: [UserCardComponent, UserFormComponent],
+  templateUrl: './user-list.component.html',
+  styleUrl: './user-list.component.css'
+})
+export class UserListComponent {
+
+  users: IUser[] = USERS;
+
+  // properties and methods
+  createUser(user: IUser) {
+    this.users.push(user);
+  }
+
+  deleteItem(id: number) {
+    let index = this.users.findIndex(x => x.id === id);
+
+    if (index !== -1)
+      this.users.splice(index, 1);
+  }
+
+  clearAll() {
+    this.users = [];
+  }
+}
